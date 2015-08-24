b412d8743dff3776da2042cf590f21233883dfc3
JEFFREY <monogut@gmail.com>
2015-08-24 14:14:12 +0800

    wire frames added, do not use this file for code

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,11 @@
 class User < ActiveRecord::Base
-  # Include default devise modules.
+
+      # Include default devise modules.
   devise :database_authenticatable, :registerable,
           :recoverable, :rememberable, :trackable, :validatable,
-          :confirmable, :omniauthable
+          :omniauthable
   include DeviseTokenAuth::Concerns::User
+   has_many :item
+   has_many :rating
+   has_many :comment
 end
