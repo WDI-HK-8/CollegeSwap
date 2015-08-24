7d9cec41da3170be1470a457a7716f9cbe0a4e4f
JEFFREY <monogut@gmail.com>
2015-08-24 15:27:32 +0800

    added schools controller GET from db

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,19 @@
 Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'auth'
 
-  resources :school do
+  resources :schools do
 	  resources :users do 
 	    resources :items do
 	    	resources :offers
 	    	resources :comments
 	    end
-	    resources :bookmarks
-	    resources :ratings
 	  end
-	end
+  end
 
 
   root 'static_pages#index'
 
   match '/swap', to: 'pages#swap', via: :get
   match '/offer', to: 'pages#offer', via: :get
-  match '/bookmark', to: 'pages#bookmark', via: :get
-
+  match '/schools', to: 'pages#schools', via: :get
 end
